0878f2ac7097b1db919c3befcfc6953694455e7f
hjshin2 <hjshin2@the-51.com>
2022-01-28 14:55:51 +0900

    모듈 구조 정리, 변경 부분 적용

--- kyobo/mok/js/ht_event_list.js
+++ kyobo/mok/js/ht_event_list.js
@@ -7,32 +7,23 @@ $(function(){
     observer: true,
     observeParents: true,
   });
+
   // 마감임박, 반응최고 스와이프
   //상품 개수 2개이하일때 ul class="evt_slider nonswipe"로 설정
-
-  if($('#swapAble1 li').length<=2){
-    $('#swapAble1 ul').removeClass('swiper-wrapper').addClass('nonswipe');
-  }else{
-    var eventSwiper1 = new CustomSwiper('#swapAble1', {
-        slidesPerView: 'auto',
-        spaceBetween: 16,
-        freeMode: true,
-        observer: true,
-        observeParents: true,
-    });
-  }
-  if($('#swapAble2 li').length<=2){
-    $('#swapAble2 ul').removeClass('swiper-wrapper').addClass('nonswipe');
-  }else{
-    var eventSwiper2 = new CustomSwiper('#swapAble2', {
-        slidesPerView: 'auto',
-        spaceBetween: 16,
-        freeMode: true,
-        observer: true,
-        observeParents: true,
-    });
-  }
-   // 오늘만 특가 스와이프
+ $('.swiper-wrapper.evt_slider').each(function(i, ele){
+    if($(ele).children('li').length <= 2){
+       $(ele).removeClass('swiper-wrapper').addClass('nonswipe');
+    }else{
+      var eventSwiper1 = new CustomSwiper('#swapAble1', {
+          slidesPerView: 'auto',
+          spaceBetween: 16,
+          freeMode: true,
+          observer: true,
+          observeParents: true,
+      });
+    }
+ });
+   // 오늘만 특가 스와이프(1개)
    var horDealSwiper = new CustomSwiper('.mySwiper.hotdeal', {
       slidesPerView: 'auto',
       spaceBetween: 12,
@@ -146,145 +137,4 @@ $(function(){
   //         });
   //     });
   // });
-
-  /*================================= HTR-M-EVT-MDL-004.html ==============================*/
-  //  배너 스와이프 
-  var evtSwiperType1, evtSwiperType2, evtSwiperType3;
-  $('.evt_banner_wrap .swiper-container').each(function (i, ele){
-    if($(ele).find('li.swiper-slide').length > 1){
-      var evtSwiperType = new CustomSwiper(this , {
-              slidesPerView: 'auto',
-              speed: 500,
-              spaceBetween: 10,
-              centeredSlides: true,
-              pagination: {
-                el: $(ele).find('.swiper-pagination')[0],
-                type : 'bullets',
-              },
-              navigation: {
-                nextEl: ".swiper-button-next",
-                prevEl: ".swiper-button-prev",
-              },
-          });
-        } else {
-          //이미지 한장인 경우 좌우 이동 버튼 안보이게, 이미지 dim 처리 안함
-          $(ele).find('.swiper-button-prev').addClass('hidden');
-          $(ele).find('.swiper-button-next').addClass('hidden');
-          $(ele).find('.banner_dim').addClass('hidden');
-      }
-
-      $(ele).find('.play_pause_box').click(function(){
-        if ( $(this).hasClass('play') ) {
-            evtSwiperType.autoplay.stop();
-            $(this).removeClass('play').addClass('pause');
-        } else {
-            evtSwiperType.autoplay.start();
-            $(this).removeClass('pause').addClass('play');
-        }
-      });  
-  });
-
-
-  /*================================= HTR-M-EVT-MDL-007.html ==============================*/  
-     //시즌 BEST 상품 배너(type_sm 140*140)
-     //상품 개수 2개이하일때 ul class 설정
-	$('.mySwiper_sm').each(function () {
-		var $parent = $(this).parent('.module_wrap');
-		if($parent.find('.swiper-slide').length > 2) {
-			var  mdlEventSwiper_sm = new CustomSwiper(this, {
-			  slidesPerView: 'auto',
-			  spaceBetween: 16,
-			  freeMode: true,
-			  observer: true,
-			  observeParents: true,
-			}); 
-		} else {
-			console.log($parent.find('.prod-wrapper').length);
-			$parent.find('.prod-wrapper').removeClass('swiper-wrapper').addClass('nonswipe');
-		}
-	  });
-  
-  /*================================= HTR-M-EVT-MDL-009-3.html ==============================*/  
-//투표 참여하기 버튼 활성화 기능(투표하기)
-  $(document).on('click', '.evt_mdl9_pop_wrap .evt_vote_list input', voteActive);
-  $('#userComment').keyup(voteActive);
-
-  function voteActive(e){
-      if(voteActiveCheck(e)){
-        $('#evtVoteBtn').removeClass('disabled');
-      }else{
-        $('#evtVoteBtn').addClass('disabled');
-      }
-  }
-
-    function voteActiveCheck(e){
-      var ckInputs = $('.evt_mdl9_pop_wrap .evt_vote_list input');
-      var ckLength = ckInputs.length;
-      var isActive = false;
-      var ckCheck = false;
-      if(e && e.target.id == 'userComment') countCommentEA(e);
-
-      for(var i=0; i<ckLength; i++){
-      if(ckInputs.eq(i).is(':checked')) {
-        ckCheck = true;
-        break;
-      }
-      }
-      if($('#userComment').val().length > 0 &&  ckCheck) isActive = true;
-      else isActive = false;
-      return isActive;
-    }
-    //댓글 입력 글자수 카운팅
-    function countCommentEA(e){
-      var length = $('#userComment').val().length;
-      if(length>300) {
-        e.preventDefault();
-        $('#userComment').val($('#userComment').val().substring(0, 300));
-        return;
-      }
-
-      $(commentEA).text(length);
-    }
-    //투표 참여 닫기 버튼 클릭 시 사용자 작성 내용 초기화
-    $('#votePopClose').click(function(){
-      var ckInputs = $('.evt_mdl9_pop_wrap .evt_vote_list input');
-      var ckLength = ckInputs.length;
-
-      for(var i=0; i<ckLength; i++){
-        ckInputs.eq(i).prop('checked', false);
-      }
-      $('#userComment').val('');
-    });
-       /*================================= HTR-M-EVT-MDL-010-1.html ============================*/
-       //응모하기 inputNum 숫자만 입력 가능 + maxlength 적용
-       var inputNums = document.querySelectorAll('.inputNum');
-
-       for(var iNum = 0; iNum<inputNums.length; iNum++){
-        inputNums[iNum].addEventListener('keyup', inpunNumCustom);
-       }
-       function inpunNumCustom(){
-         var numCheck = /[~!@#$%^&*()_+|<>?:{}|ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;; 
-         var max = this.getAttribute('maxlength');
-         if(max > 0 && this.value.length > max) this.value  = this.value.slice(0, max);
-         if(numCheck.test(this.value)) this.value  = this.value.replace(type, '');
-       }
-
-      //  if(document.querySelector('#evt_apply_wrap')){
-      //    var applyInputs = document.querySelectorAll('.evt_apply_wrap input');
-      //    for(var ckIp = 0; ckIp<applyInputs.length; ckIp++){
-      //     applyInputs[ckIp].addEventListener('keyup', activeBtn);
-      //    }
-        
-      //    function activeBtn(){
-      //      var active = false;
-      //      for(var ckIp = 0; ckIp<applyInputs.length; ckIp++){
-      //       applyInputs[ckIp].addEventListener('keyup', activeBtn);
-      //      }
-      //    }
-       //} 
-    /*================================= HTR-M-EVT-VIEW-019.html ==============================*/  
-    //댓글 입력 이미지 리스트 활성화 
-    $(document).on('click', '.cmt_imgs_list .cmt_img_item', function(){
-       $(this).removeClass('active_non').addClass('active').siblings('.cmt_img_item').removeClass('active').addClass('active_non');
-    });
 });
