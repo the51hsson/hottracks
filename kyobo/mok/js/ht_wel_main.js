ce78e6b57b7365b476285d99185548c51958f0cc
JH <jhicbw@gmail.com>
2022-02-10 13:03:43 +0900

    22.02.10 라이브 메인 작업중

--- kyobo/mok/js/ht_wel_main.js
+++ kyobo/mok/js/ht_wel_main.js
@@ -718,3 +718,21 @@ $(function(){
 
 
 
+// 미리 만나는 라이브
+function livNewMsg(){
+	var offsetPosition = $('.liv_list .li:first-child .alarm_chk02').offset();
+	var y = offsetPosition.top - $('.liv_list').offset().top + 35;
+	$('.liv_new_msg').css('top', y);
+	console.log(y)
+
+}
+$(function(){
+    if(!$('.liv_new_msg').length) return;
+	livNewMsg();
+	$(window).resize(function() {
+		livNewMsg();
+	});
+});
+
+
+
